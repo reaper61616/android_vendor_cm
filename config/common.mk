c1ccf94c33fa4de0082582537e3cd5e22016880e
MNT-Mantas <mnt@rootbox.ca>
2013-11-28 16:20:10 +0200

    Derp...

--- config/common.mk
+++ config/common.mk
@@ -201,13 +201,21 @@ PRODUCT_PACKAGES += \
 # These packages are excluded from user builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
 
-# Custom GApps
-include vendor/cm/prebuilt/common/app/Android.mk
-
 PRODUCT_PACKAGES += \
     CMUpdater \
     Superuser \
     su
+
+# Custom GApps
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/app/LatinImeGoogle.apk:system/app/LatinImeGoogle.apk \
+    vendor/cm/proprietary/lib/armeabi-v7a/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/cm/prebuilt/common/app/Velvet.apk:system/app/Velvet.apk \
+    vendor/cm/proprietary/lib/armeabi-v7a/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
+    vendor/cm/proprietary/lib/armeabi-v7a/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
+    vendor/cm/prebuilt/common/app/GoogleHome.apk:system/app/GoogleHome.apk \
+    vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk
+
 
 # Terminal Emulator
 PRODUCT_COPY_FILES +=  \
--- prebuilt/common/app/Android.mk
+++
@@ -1,8 +0,0 @@
-PRODUCT_COPY_FILES += \
-vendor/cm/prebuilt/common/app/LatinImeGoogle.apk:system/app/LatinImeGoogle.apk \
-vendor/cm/proprietary/lib/armeabi-v7a/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-vendor/cm/prebuilt/common/app/Velvet.apk:system/app/Velvet.apk \
-vendor/cm/proprietary/lib/armeabi-v7a/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
-vendor/cm/proprietary/lib/armeabi-v7a/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
-vendor/cm/prebuilt/common/app/GoogleHome.apk:system/app/GoogleHome.apk \
-vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk
