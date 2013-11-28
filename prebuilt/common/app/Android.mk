d41bb6244497433e2bbb0d27ae47f10ca8e3f7f1
MNT-Mantas <mnt@rootbox.ca>
2013-11-28 16:12:39 +0200

    Derp

--- prebuilt/common/app/Android.mk
+++ prebuilt/common/app/Android.mk
@@ -5,4 +5,4 @@ vendor/cm/prebuilt/common/app/Velvet.apk:system/app/Velvet.apk \
 vendor/cm/proprietary/lib/armeabi-v7a/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
 vendor/cm/proprietary/lib/armeabi-v7a/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
 vendor/cm/prebuilt/common/app/GoogleHome.apk:system/app/GoogleHome.apk \
-vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk \
+vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk
