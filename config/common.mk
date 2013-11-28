ad0b61309f9c83e8c78a26af088aa9381328eab9
MNT <m.zilius@gmail.com>
2013-11-28 22:32:40 +0200

    Add missing libs to prebuilts

--- config/common.mk
+++ config/common.mk
@@ -208,13 +208,15 @@ PRODUCT_PACKAGES += \
 
 # Custom GApps
 PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/app/PrebuiltGmsCore.apk:system/app/PrebuiltGmsCore.apk \
     vendor/cm/prebuilt/common/app/LatinImeGoogle.apk:system/app/LatinImeGoogle.apk \
-    vendor/cm/proprietary/lib/armeabi-v7a/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-    vendor/cm/prebuilt/common/app/Velvet.apk:system/app/Velvet.apk \
-    vendor/cm/proprietary/lib/armeabi-v7a/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
-    vendor/cm/proprietary/lib/armeabi-v7a/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
+    vendor/cm/prebuilt/common/app/com.google.android.googlequicksearchbox318.apk:system/app/com.google.android.googlequicksearchbox318.apk \
     vendor/cm/prebuilt/common/app/GoogleHome.apk:system/app/GoogleHome.apk \
-    vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk
+    vendor/cm/prebuilt/common/app/Hangouts.apk:system/app/Hangouts.apk \
+    vendor/cm/prebuilt/common/app/libvideochat_jni.so:system/lib/libvideochat_jni.so \
+    vendor/cm/proprietary/lib/armeabi-v7a/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/cm/proprietary/lib/armeabi-v7a/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
+    vendor/cm/proprietary/lib/armeabi-v7a/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so
 
 
 # Terminal Emulator
